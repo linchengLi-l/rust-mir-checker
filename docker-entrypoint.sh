62ed37ac6b0b96dc9d335b9284cffd58ba9c1a74
snowdream <sn0wdr1am@qq.com>
2024-10-17 19:28:16 +0800

    feat: LD_LIBRARY_PATH

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,7 @@ cd /root/rust-mir-checker
 . "$HOME/.cargo/env"
 export LIBCLANG_PATH=/opt/clang+llvm-15.0.6-x86_64-linux-gnu-ubuntu-18.04/lib/libclang.so
 export PATH=/opt/clang+llvm-15.0.6-x86_64-linux-gnu-ubuntu-18.04/bin:$PATH
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH":"$HOME"/.rustup/toolchains/nightly-2020-12-29-x86_64-unknown-linux-gnu/lib
 export RUSTFLAGS="-Clink-args=-fuse-ld=lld"
 cargo build --verbose
 
